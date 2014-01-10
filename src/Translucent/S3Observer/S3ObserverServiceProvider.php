64e9d1a7bf435a35ea28f96a47eddb6aaf73ba93
Kento <kirkland1739@gmail.com>
2014-01-10 17:32:37 +0900

    Initial commit

---
+++ src/Translucent/S3Observer/S3ObserverServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Translucent\S3Observer;
+
+use Illuminate\Support\ServiceProvider;
+
+class S3ObserverServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
